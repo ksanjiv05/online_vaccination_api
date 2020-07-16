640e0b71b44f8d752e1334dc5b8a927c5bac5629
sanjiv kumar <ksanjiv0005@gmail.com>
2020-07-16 11:33:36 +0530

    add New Applicant update

--- src/components/dashboard/new_applicant/NewApplicant.js
+++ src/components/dashboard/new_applicant/NewApplicant.js
@@ -5,89 +5,91 @@ const NewApplicant = () => {
   const changeHandler = (ev) => {};
   return (
     <div className="dashboard-background">
-      <div className="dashboard-containt">
-        <form onSubmit={(ev) => handleSubmit(ev)}>
-          <div className="input-group">
-            <span className="input-group-addon">
-              <i className="glyphicon glyphicon-user"></i>
-            </span>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter Applicant Name"
-              name="name"
-              onChange={(ev) => changeHandler(ev)}
-            />
-          </div>
-          <div className="input-group">
-            <span className="input-group-addon">
-              <i className="glyphicon glyphicon-calendar"></i>
-            </span>
-            <input
-              type="date"
-              className="form-control"
-              placeholder="Enter Applicant DOB"
-              name="dob"
-              onChange={(ev) => changeHandler(ev)}
-            />
-          </div>
-          <div className="input-group">
-            <span className="input-group-addon">
-              <i className="glyphicon glyphicon-user"></i>
-            </span>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter Father's Name"
-              name="father"
-              onChange={(ev) => changeHandler(ev)}
-            />
-          </div>
-          <div className="input-group">
-            <span className="input-group-addon">
-              <i className="glyphicon glyphicon-user"></i>
-            </span>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Mother's Name "
-              name="father"
-              onChange={(ev) => changeHandler(ev)}
-            />
-          </div>
+      <center>
+        <div className="dashboard-containt">
+          <form onSubmit={(ev) => handleSubmit(ev)}>
+            <div className="input-group">
+              <span className="input-group-addon">
+                <i className="glyphicon glyphicon-user"></i>
+              </span>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Applicant Name"
+                name="name"
+                onChange={(ev) => changeHandler(ev)}
+              />
+            </div>
+            <div className="input-group">
+              <span className="input-group-addon">
+                <i className="glyphicon glyphicon-calendar"></i>
+              </span>
+              <input
+                type="date"
+                className="form-control"
+                placeholder="Enter Applicant DOB"
+                name="dob"
+                onChange={(ev) => changeHandler(ev)}
+              />
+            </div>
+            <div className="input-group">
+              <span className="input-group-addon">
+                <i className="glyphicon glyphicon-user"></i>
+              </span>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Father's Name"
+                name="father"
+                onChange={(ev) => changeHandler(ev)}
+              />
+            </div>
+            <div className="input-group">
+              <span className="input-group-addon">
+                <i className="glyphicon glyphicon-user"></i>
+              </span>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Mother's Name "
+                name="father"
+                onChange={(ev) => changeHandler(ev)}
+              />
+            </div>
 
-          <div className="input-group">
-            <span className="input-group-addon">
-              <i className="glyphicon glyphicon-edit"></i>
-            </span>
+            <div className="input-group">
+              <span className="input-group-addon">
+                <i className="glyphicon glyphicon-edit"></i>
+              </span>
+              <input
+                type="number"
+                className="form-control"
+                name="weight"
+                placeholder="Enter Applicant Weight(KG)"
+                onChange={(ev) => changeHandler(ev)}
+              />
+            </div>
+            <div className="input-group">
+              <span className="input-group-addon">
+                <i className="glyphicon glyphicon-edit"></i>
+              </span>
+              <input
+                type="number"
+                className="form-control"
+                name="height"
+                placeholder="Enter Applicant Height(CM) "
+                onChange={(ev) => changeHandler(ev)}
+              />
+            </div>
+            <br />
             <input
-              type="number"
-              className="form-control"
-              name="weight"
-              placeholder="Enter Applicant Weight(KG)"
-              onChange={(ev) => changeHandler(ev)}
+              type="submit"
+              className="btn btn-success form-control"
+              value="Change Password"
             />
-          </div>
-          <div className="input-group">
-            <span className="input-group-addon">
-              <i className="glyphicon glyphicon-edit"></i>
-            </span>
-            <input
-              type="number"
-              className="form-control"
-              name="height"
-              placeholder="Enter Applicant Height(CM) "
-              onChange={(ev) => changeHandler(ev)}
-            />
-          </div>
-          <br />
-          <input
-            type="submit"
-            className="btn btn-success form-control"
-            value="Change Password"
-          />
-        </form>
-      </div>
+          </form>
+        </div>
+      </center>
     </div>
   );
 };
