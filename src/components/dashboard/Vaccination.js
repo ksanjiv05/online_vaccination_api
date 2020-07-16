9c269db5ecd0015d9a89a821b0a22205fd31cf63
sanjiv kumar <ksanjiv0005@gmail.com>
2020-07-16 11:28:50 +0530

    add New Applicant

--- src/components/dashboard/Vaccination.js
+++ src/components/dashboard/Vaccination.js
@@ -1,90 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import VaccinationTab from "./VaccinationTab";
+import NewApplicant from "./new_applicant/NewApplicant";
 
 const Vaccination = () => {
+  const [tabState, setTabState] = useState(false);
+  const handelTab = () => {
+    setTabState(true);
+  };
   return (
-    <VaccinationTab />
-
-    // <div className="vacc-background">
-    //   <ul className="nav nav-tabs">
-    //     <li className="active">
-    //       <a data-toggle="tab" href="#home">
-    //         Sanjiv
-    //       </a>
-    //     </li>
-    //     <li>
-    //       <a data-toggle="tab" href="#menu1">
-    //         Vinay
-    //       </a>
-    //     </li>
-    //     <li>
-    //       <a data-toggle="tab" href="#menu2">
-    //         Sohan
-    //       </a>
-    //     </li>
-    //   </ul>
-
-    //   <div className="tab-content">
-    //     <div id="home" className="tab-pane fade in active">
-    //       <div className="vacc-div">
-    //         <h2 className="vacc-font">
-    //           Upcoming Vaccination<span className="badge badge-ext">ORM</span>
-    //         </h2>
-    //         <div className="badge badge-date">02/09/2020</div>
-    //       </div>
-    //       <div className="vacc-div">
-    //         <h2 className="vacc-font">Recent vacciation</h2>
-    //         <div className="badge badge-date">02/09/2020</div>
-    //       </div>
-    //       <div className="vacc-div-ex">
-    //         <div>
-    //           <table className="table table-striped">
-    //             <thead>
-    //               <tr>
-    //                 <th>Firstname</th>
-    //                 <th>Lastname</th>
-    //                 <th>Email</th>
-    //               </tr>
-    //             </thead>
-    //             <tbody>
-    //               <tr>
-    //                 <td>John</td>
-    //                 <td>Doe</td>
-    //                 <td>john@example.com</td>
-    //               </tr>
-    //               <tr>
-    //                 <td>Mary</td>
-    //                 <td>Moe</td>
-    //                 <td>mary@example.com</td>
-    //               </tr>
-    //               <tr>
-    //                 <td>July</td>
-    //                 <td>Dooley</td>
-    //                 <td>july@example.com</td>
-    //               </tr>
-    //             </tbody>
-    //           </table>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div id="menu1" className="tab-pane fade"></div>
-    //     <div id="menu2" className="tab-pane fade"></div>
-    //   </div>
-    // </div>
-    /////////////////////////////
-
-    // <div className="vacc-background">
-    //   <div className="vacc-div">
-    //     <h2 className="vacc-font">Upcoming Vaccination</h2>
-    //   </div>
-    //   <div className="vacc-div">
-    //     <h2 className="vacc-font">Recent vacciation</h2>
-    //   </div>
-    //   <div className="vacc-div-ex">
-    //     <div></div>
-    //   </div>
-    //   <div></div>
-    // </div>
+    <>
+      {tabState ? (
+        <>
+          <div className="new-applicant">
+            <button
+              className="btn btn-success form-control"
+              onClick={() => setTabState(false)}>
+              <span class="glyphicon glyphicon-arrow-left"></span> Back To Main
+            </button>
+          </div>
+          <NewApplicant />
+        </>
+      ) : (
+        <>
+          <div className="new-applicant">
+            <button
+              className="btn btn-success form-control"
+              onClick={handelTab}>
+              <span class="glyphicon glyphicon-plus"></span> Add New Applicant
+            </button>
+          </div>
+          <VaccinationTab />
+        </>
+      )}
+    </>
   );
 };
 export default Vaccination;
